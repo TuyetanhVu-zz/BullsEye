b022a6390f801cf728e6eb9dbc8c1392bd1cd806
Tuyet <tuyet22.vu@gmail.com>
2017-12-28 20:42:31 -0500

    This is the first version of the Bull's Eye app

---
+++ BullsEye/BullsEye/AuthorViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AboutAuthorViewController.swift
+//  BullsEye
+//
+//  Created by Tuyetanh Vu on 12/20/17.
+//  Copyright © 2017 Tuyetanh Vu. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    @IBAction func closeTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
